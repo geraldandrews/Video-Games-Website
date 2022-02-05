748b4a5ad4152133693cc94edfe951e301c6df84
Gerald Andrews <gerald2399@comcast.net>
2022-02-05 16:47:54 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+const hamburger = document.getElementById('hamburger');
+
+hamburger.addEventListener('click', function(e) {
+  hamburger.classList.toggle('cross');
+});
+
+navToggle.addEventListener("click", function () {
+  // console.log(links.classList);
+  // console.log(links.classList.contains("random"));
+  // console.log(links.classList.contains("links"));
+   if (links.classList.contains("show-links")) {
+     links.classList.remove("show-links");
+   } else {
+     links.classList.add("show-links");
+   }
+  //links.classList.toggle("show-links"); 
+});
