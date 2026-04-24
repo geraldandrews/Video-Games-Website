a9216829603f7a2b9b89d245369692d277b16d0f
Gerald Andrews <gerald2399@comcast.net>
2026-04-23 22:01:44 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Navbar
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 const hamburger = document.getElementById('hamburger');
@@ -7,13 +8,37 @@ hamburger.addEventListener('click', function(e) {
 });
 
 navToggle.addEventListener("click", function () {
-  // console.log(links.classList);
-  // console.log(links.classList.contains("random"));
-  // console.log(links.classList.contains("links"));
    if (links.classList.contains("show-links")) {
      links.classList.remove("show-links");
    } else {
      links.classList.add("show-links");
    }
-  //links.classList.toggle("show-links"); 
 });
+
+// Modal
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-btn');
+const gameImgs = document.querySelectorAll('.game-img')
+
+gameImgs.forEach(img => {
+  img.addEventListener('click', async (e) => {
+    const clickedId = e.target.getAttribute('data-id');
+
+    const response = await fetch('games.json');
+    const data = await response.json();
+
+    const item = data.find(obj => obj.id == clickedId);
+
+    document.querySelector('.modal-img').src = item.image;
+    document.querySelector('.modal-title').innerText = item.title;
+    document.querySelector('.release-date').innerText = item.release;
+    document.querySelector('.console').innerText = item.console;
+
+    modal.style.display = 'flex';
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+  
